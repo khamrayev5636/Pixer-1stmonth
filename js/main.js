2e3f1dd46e4d6bebb2fcc1d2a83793eea065ee1c
khamrayev5636 <khamrayev5636@gmail.com>
2022-11-25 18:12:26 +0500

    Add brand section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const elBurgerBtn = document.querySelector(".header__open-js");
+const elBody = document.querySelector("body");
 
 elBurgerBtn.addEventListener("click" , ()=> {
-    elBurgerBtn.closest(".header").classList.toggle("shownav")
+    elBurgerBtn.closest(".header").classList.toggle("shownav");
 })
+
+elBurgerBtn.addEventListener("click" , ()=> {
+    elBody.classList.add("shownav-js")
+});
+
+elBurgerBtn.addEventListener("click" , ()=> {
+    elBody.classList.remove("shownav-js")
+});
